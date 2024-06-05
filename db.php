c79425c801ba3cd948b448d4e73b6857b6193b81
Tausif Rahman <69134814+tawsif0@users.noreply.github.com>
2024-06-06 04:12:28 +0600

    Updated

--- db.php
+++ db.php
@@ -1,13 +1,13 @@
 <?php
 $servername = "localhost";
-$username = "root";  // Your database username
-$password = "";  // Your database password
-$dbname = "mywebsite";  // Your database name
+$username = "root";  
+$password = "";  
+$dbname = "mywebsite"; 
 
-// Create connection
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
